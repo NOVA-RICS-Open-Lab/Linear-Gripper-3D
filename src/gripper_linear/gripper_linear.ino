daa44f7a2c0dd45002f504e755a2df049950adb2
Raúl Mestre Dinis <102921290+ZeRMD@users.noreply.github.com>
2025-04-28 03:47:13 +0100

    txoma

--- src/gripper_linear/gripper_linear.ino
+++ src/gripper_linear/gripper_linear.ino
@@ -1,7 +1,7 @@
 #include <AccelStepper.h> // Found in Arduino Library or https://www.airspayce.com/mikem/arduino/AccelStepper/
 #include "secrets.h" // Containing WiFi Information
 #include <WiFi.h>
-
+#include <WebServer.h>
 
 #define dir 33
 #define step 32
@@ -23,7 +23,7 @@ char password [] = SECRET_PASS;   // your network password
 int keyIndex = 0;            // your network key index number (needed only for WEP)
 
 // Set web server port number to 80
-WiFiServer server(80);
+WebServer server(80);
 // Variable to store the HTTP request
 String header;
 // Set your Static IP address
@@ -32,11 +32,6 @@ IPAddress local_IP(192, 168, 1, 2);
 IPAddress gateway(192, 168, 250, 1);
 IPAddress subnet(255, 255, 0, 0);
 
-String readString;
-// Client variables 
-String response;
-String dimension;
-
 // Current time
 unsigned long currentTime = millis();
 // Previous time
@@ -44,27 +39,11 @@ unsigned long previousTime = 0;
 // Define timeout time in milliseconds (example: 2000ms = 2s)
 const long timeoutTime = 2000;
 
+//////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////// Setup /////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////
 
-int calcSteps(int dimension){
-  if (dimension > MAX_DIST) {
-    return 0;
-  }
-  else {
-     int steps = 0;
-    int travel=0;
-    travel = MAX_DIST-dimension;
-    Serial.println(travel);
-    steps=travel/DIST_PER_STEP;
-    return steps;
-  }
- 
-}
-
-
-void setup() {
-  // Open serial communications and wait for port to open:
-  Serial.begin(9600);
-
+void SetupWifi() {
   // Configures static IP address
   if (!WiFi.config(local_IP, gateway, subnet)) {
     Serial.println("STA Failed to configure");
@@ -82,111 +61,96 @@ void setup() {
   Serial.println("WiFi connected.");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
-  server.begin();
+}
 
+void SetupWiFiServer() {
+
+  server.on("/open/", HTTP_POST, HandleOPEN);
+  server.on("/close/", HTTP_POST, HandleCLOSE);
+  server.onNotFound(HandleNotFound);
+  
+  server.begin();
+}
+
+void SetupStepper() {
   stepper.setCurrentPosition(2223);
   stepper.setMaxSpeed(2000); //SPEED Steps / second
   stepper.setAcceleration(1000); // ACCELERATION Steps/(second)^2
-  
 }
 
+void setup() {
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+
+  SetupWifi();
+  SetupWiFiServer();
+  SetupStepper();
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////// Loop //////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////
+
 void loop() {
+  server.handleClient();
+  delay(1);
+}
 
-  WiFiClient client = server.available();   // Listen for incoming clients
+//////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////// Server Handlers /////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////
 
-  if (client) {                             // If a new client connects,
-    Serial.println("New Client.");          // print a message out in the serial port
-    String currentLine = "";                // make a String to hold incoming data from the client
-    currentTime = millis();
-    previousTime = currentTime;
-    while (client.connected() && currentTime - previousTime <= timeoutTime) { // loop while the client's connected
-      currentTime = millis();         
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        Serial.write(c);                    // print it out the serial monitor
-        readString += c;
-        if (c == '\n') {                    // if the byte is a newline character
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
-          if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println("Connection: close");
-            client.println();
-            
-            if (readString.indexOf("/close") >0){
-            Serial.print(readString);
-            if (readString.indexOf("?dimension=") >0){    
-              int pos1 = readString.indexOf("=");
-              Serial.println(pos1);              
-              int pos2 = readString.indexOf("HTTP");
-              Serial.println(pos2); 
-              dimension = readString.substring(pos1+1,pos2-1);
-              Serial.print("dimension:");
-              Serial.println(dimension);
-              response= "skill close done";
-              int x=dimension.toInt();
-              int steps = 0;
-              steps=calcSteps(x);
-              if (steps == 0){
-                Serial.println ("Nao é possivel apanhar uma peça desse tamanho");
-                response= "skil close - the dimension is to big";
-              }
-              else{
-                Serial.println(steps);
-                stepper.runToNewPosition(steps);
-                delay(5000);
-                response = "skill close done";
-              }
-              
-            }                      
-          }
-          if (readString.indexOf("/open") >0){
-            //Serial.print(readString);
-            Serial.println("Skill open done");
-            stepper.runToNewPosition(0);
-            response= "skill open done"; 
-          }
-            
-            // The HTTP response ends with another blank line
-            client.println(response);
-            // Break out of the while loop
-            break;
-          } else { // if you got a newline, then clear currentLine
-            currentLine = "";
-          }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
-        }
-      }
-    }
-    // Clear the header variable
-    readString = "";
-    // Close the connection
-    client.stop();
-    Serial.println("Client disconnected.");
-    Serial.println("");
-  }
-  /*
+void HandleNotFound() {
+  server.send(200, "text/plain", "SKILL NOT FOUND");
+}
+
+void HandleOPEN() {
   stepper.runToNewPosition(0);
-  delay(5000);
+  server.send(200, "text/plain", "Skill OPEN: Done");
+}
+
+void HandleCLOSE() {
+
+  String response;
+  String dimension;
+
+  if (server.hasArg("dimension")) {
+    dimension = server.arg("dimension");
+  }
+
+  int x=dimension.toInt();
   int steps = 0;
-  steps=calcSteps(58);
+  steps=CalcSteps(x);
+
   if (steps == 0){
-    Serial.println ("Nao é possivel apanhar uma peça desse tamanho");
+    Serial.println ("Not possible to pickup a piece that big");
+    response= "The dimension is to big";
   }
   else{
     Serial.println(steps);
     stepper.runToNewPosition(steps);
     delay(5000);
+    response = "Done";
   }
-  
 
-  delay(5000);
-  stepper.runToNewPosition(0);
-  delay(5000);
-  stepper.runToNewPosition(2223);
-  delay(5000);*/
+  server.send(200, "text/plain", "Skill CLOSE: " + response);
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////// Auxiliary Functions ////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+int CalcSteps(int dimension){
+  if (dimension > MAX_DIST) {
+    return 0;
+  }
+  else {
+     int steps = 0;
+    int travel=0;
+    travel = MAX_DIST-dimension;
+    Serial.println(travel);
+    steps=travel/DIST_PER_STEP;
+    return steps;
+  }
+ 
 }
